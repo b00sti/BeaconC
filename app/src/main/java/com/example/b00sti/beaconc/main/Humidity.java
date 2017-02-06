c4925fb3eb7eef65b603efab11db9a8d4f49a878
b00sti <pawlik.dominik@gmail.com>
2017-02-06 18:01:23 +0100

    very simple lombok test

---
+++ app/src/main/java/com/example/b00sti/beaconc/main/Humidity.java
@@ -0,0 +1,13 @@
+package com.example.b00sti.beaconc.main;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Dominik (b00sti) Pawlik on 2017-02-06
+ */
+
+@Getter
+@Setter
+public class Humidity {
+}
